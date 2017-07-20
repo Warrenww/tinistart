65a24e4839384892a3c46eeb065854323b45f1c6
Warrenww <warrenpig@gmail.com>
2017-07-20 22:59:19 +0800

    'wwwwww'

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 
   window.onscroll = function() {scrollFunction()};
-
+  var w = window.innerWidth;
   function scrollFunction() {
+    var but = document.getElementById("nav-buttom")
       if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
           document.getElementById("nav").style.opacity = .9;
-          document.getElementById("nav").style.transition = "opacity .8s";
-          document.getElementById("Top").style.opacity = .9;
-          document.getElementById("Top").style.transition = "opacity .8s";
+          document.getElementById("top").style.opacity = .9;
+          but.style.backgroundColor = 'rgba(0,0,0,0.35)';
+          but.style.top = '10px';
+
       } else {
           document.getElementById("nav").style.opacity = 0;
-          document.getElementById("Top").style.opacity = 0;
+          document.getElementById("top").style.opacity = 0;
+          but.style.backgroundColor = 'rgba(0,0,0,0)';
+          but.style.top = 0;
       }
     }
 
@@ -30,25 +34,24 @@
         main = document.getElementById('nav-buttom'),
         column = document.getElementById('mobile-nav-container'),
         main = document.getElementById('mobile-nav'),
-        org = document.getElementById('original-nav'),
-        top = document.getElementById('Top');
+        top = document.getElementById('top');
 
     if(rotated){
-      div1.style.top = div2.style.top = div3.style.top = '20px';
+      div1.style.top = div2.style.top = div3.style.top = '25px';
       div1.style.transform   = 'rotate(45deg)';
       div2.style.transform   = 'rotate(-45deg)';
-      div3.style.opacity = org.style.opacity = 0;
+      div3.style.opacity = 0;
       column.style.right = '0%';
       main.style.zIndex = '100' ;
       main.style.backgroundColor = 'rgba(0,0,0,.5)';
       top.style.display = 'none';
     }else {
       div1.style.transform  = div2.style.transform = 'rotate(0deg)';
-      div1.style.top = '20%';
-      div2.style.top = '40%' ;
-      div3.style.top = '60%' ;
-      div3.style.opacity = org.style.opacity = 1;
-      column.style.right = '-40%';
+      div1.style.top = '15px';
+      div2.style.top = '23.75px' ;
+      div3.style.top = '32.5px' ;
+      div3.style.opacity = 1;
+      column.style.right = '-200px';
       setTimeout(
         function(){
         main.style.zIndex = '-1';
@@ -61,48 +64,45 @@
     rotated = !rotated;
   }
 
-  var h = window.innerHeight,
-      stop = 0 ;
-  function gop2() {
-    if(stop<h){
-      window.scrollBy(0,15);
-      timeOut=setTimeout('gop2()',5);
-      stop += 15 ;
-    }
-    else {
-      clearTimeout(timeOut);
-      stop = 0;
-    }
-  }
-  function gop2F() {
-    if(stop<h*0.75){
-      window.scrollBy(0,1);
-      timeOut=setTimeout('gop2F()',5);
-      stop += 1 ;
-    }
-    else {
-      clearTimeout(timeOut);
-      stop = 0;
-    }
-  }
-  function goto(a,b) {
-    var aa = document.getElementById(a),
-        bb = document.getElementById(b);
-    var pa = getposition(aa),
-        pb = getposition(bb);
-    alert(pb-pa);
 
-  }
-  function getposition(element) {
-    var data = element.getBoundingClientRect();
-    return data.offset.top;
-  }
-
-  function scroll_to_div(div_id)
-  {
+  function scroll_to_div(div_id){
    $('html,body').animate(
-   {
-    scrollTop: $("#"+div_id).offset().top
-   },
-   'ease-in-out');
+   {scrollTop: $("#"+div_id).offset().top},
+   1000,'easeInOutCubic');
+  }
+
+
+  var i=0;
+  function toggle(div_id) {
+    if(i<$('#'+div_id+' li').length){
+      document.getElementsByTagName('button')[1].disabled = 1;
+      document.getElementsByTagName('button')[2].disabled = 1;
+      $("#"+div_id+' li:eq('+i+')').toggle(300,'easeInOutCubic',function(){i++;toggle(div_id);});
+    }else {
+      i = 0;
+      document.getElementsByTagName('button')[1].disabled = 0;
+      document.getElementsByTagName('button')[2].disabled = 0;
+    }
+  }
+
+  function rotate(div_id) {
+    var d = document.getElementById(div_id);
+    ang = getRotationDegrees($('#'+div_id));
+    ang += 180;
+    d.style.transform  = 'rotate('+ang+'deg)' ;
+  }
+
+  function getRotationDegrees(obj) {
+    var matrix = obj.css("-webkit-transform") ||
+    obj.css("-moz-transform")    ||
+    obj.css("-ms-transform")     ||
+    obj.css("-o-transform")      ||
+    obj.css("transform");
+    if(matrix !== 'none') {
+        var values = matrix.split('(')[1].split(')')[0].split(',');
+        var a = values[0];
+        var b = values[1];
+        var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+    } else { var angle = 0; }
+    return (angle < 0) ? angle + 360 : angle;
   }
